df472b9a04e490fc5789a319e1af99693494ed13
Juan David Garrido <jgarrido@mscs241-28463.olinriclab.macalester.edu>
2018-11-10 01:54:25 -0600

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,8 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
-  { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', loadChildren: '../pages/login/login.module#LoginPageModule' },
 ];
 
 @NgModule({
