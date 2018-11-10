b676c0b931d951cc85bafa9eb4f8e68e06f068e9
garri1105 <juandagarrido@hotmail.com>
2018-11-10 06:36:34 -0600

    Smooth progress bar

--- src/pages/setup-pair/setup-pair.page.ts
+++ src/pages/setup-pair/setup-pair.page.ts
@@ -1,29 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-setup-pair',
   templateUrl: './setup-pair.page.html',
   styleUrls: ['./setup-pair.page.scss'],
 })
-export class SetupPairPage implements OnInit {
+export class SetupPairPage implements OnInit, OnDestroy {
   completion: number;
-
+  interval: any;
   constructor() {}
 
   pairToDevice() {
-    setTimeout(()=>{
-      this.completion = 50;
-    }, 500);
-    setTimeout(()=>{
-      this.completion = 100;
-    }, 1000);
-    setTimeout(()=>{
-      window.location.href = "/setupColor";
-    }, 1500);
+    this.interval = setInterval(() => {
+        if (this.completion < 100) {
+            this.completion += 1;
+        } else {
+            window.location.href = "/setupColor";
+        }
+      }, 15);
+
+    // setTimeout(()=>{
+    //   this.completion = 50;
+    // }, 500);
+    // setTimeout(()=>{
+    //   this.completion = 100;
+    // }, 1000);
+    // setTimeout(()=>{
+    //   window.location.href = "/setupColor";
+    // }, 1500);
   }
 
   ngOnInit() {
     this.completion = 0;
+  }
+
+  ngOnDestroy() {
+      clearInterval(this.interval);
   }
 
 }
