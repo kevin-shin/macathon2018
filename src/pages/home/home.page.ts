7d131b89c04cdef5d73d7a9d16f786160bf7fcad
garri1105 <juandagarrido@hotmail.com>
2018-11-10 06:19:08 -0600

    Added timer

--- src/pages/home/home.page.ts
+++ src/pages/home/home.page.ts
@@ -1,14 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
 })
-export class HomePage implements OnInit {
+export class HomePage implements OnInit, OnDestroy {
   drinkCurrent: number;
   drinkLimit: number;
   color: string;
+  time: string;
+  timer: any;
+  lastDrinkTime: any;
 
   constructor() { }
 
@@ -22,6 +25,7 @@ export class HomePage implements OnInit {
   }
 
   increaseDrinks() {
+    this.lastDrinkTime = new Date();
     this.drinkCurrent += 1;
     if (this.drinkCurrent >= 10) {
         this.color = '#AA3333'
@@ -37,10 +41,28 @@ export class HomePage implements OnInit {
     }
   }
 
+  pad(n){return n<10 ? '0'+n : n}
+
   ngOnInit() {
+    this.timer = setInterval(() => {
+        if (this.lastDrinkTime) {
+            let lastDrinkTime = this.lastDrinkTime.getTime();
+            let now = new Date().getTime();
+            let time = new Date(now - lastDrinkTime);
+            this.time = `${this.pad(time.getMinutes())}:${this.pad(time.getSeconds())}`;
+        }
+        else {
+            this.time = '00:00'
+        }
+    }, 300);
+
     this.drinkCurrent = 2;
     this.drinkLimit = 9;
     this.color='#0F7722';
     window.addEventListener('keydown', this.increaseDrinks.bind(this));
+  }
+
+  ngOnDestroy() {
+      clearInterval(this.timer);
   }
 }
